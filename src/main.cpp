23924fd0a60f922488bb102da38ece66ae79bc9a
VenousCoot <sfs6304@psu.edu>
2024-12-07 13:59:39 -0500

    Intro dialog

---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+int main()
+{
+	cout <<"Welcome to Hell Adventurer, this is Newark Liberty International Airport!" << endl;
+	cout <<"Voted the worst airport in the United States according to AirHelp's Global Airport Ranking (2019)." << endl;
+	cout <<"It looks like you have a flight departing in ... 1 hour!! Better Hurry!" << endl;
+
+
+	return 0; 
+}
