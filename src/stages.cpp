12ad3e6e95075fa618dfddb27961ce424a06a675
VenousCoot <sfs6304@psu.edu>
2024-12-09 15:15:10 -0500

    Me baby
    swows

--- src/stages.cpp
+++ src/stages.cpp
@@ -211,11 +211,11 @@ Gate::Gate(bool hasFirstClassBoardingPass) : hasFirstClassBoardingPass(hasFirstC
 }
 
 void Gate::enter() {
-    cout << description << endl;
-    cout << "You actually managed to make it to your gate despite the extreme violence you have shown during our time together. Or maybe I should say that you made it this far THANKS to the violence." << endl;
-    cout << "Just go up to the front and wait for your group to be called, and please, for the love of all that is good, do not start a fight." << endl;
-    cout << npcName << ": Now boarding groups 1 and 2! Now boarding groups 1 and 2!" << endl;
-    cout << "Excuse me, may I see your boarding pass?" << endl;
+    printLetterByLetter( description );
+    printLetterByLetter( "You actually managed to make it to your gate despite the extreme violence you have shown during our time together. Or maybe I should say that you made it this far THANKS to the violence." );
+    printLetterByLetter( "Just go up to the front and wait for your group to be called, and please, for the love of all that is good, do not start a fight." );
+    printLetterByLetter( npcName + ": Now boarding groups 1 and 2! Now boarding groups 1 and 2!" );
+    printLetterByLetter( "Excuse me, may I see your boarding pass?" );
 
     string playerInput;
     bool validInput = false;
@@ -231,7 +231,7 @@ void Gate::enter() {
         getline(cin, playerInput);
 
         if (playerInput == "1") {
-            cout << npcName << ": How did you even get here without a boarding pass?" << endl;
+            printLetterByLetter(npcName + ": How did you even get here without a boarding pass?" );
 
             validInput = false;
             while (!validInput) {
@@ -241,21 +241,21 @@ void Gate::enter() {
                 getline(cin, playerInput);
 
                 if (playerInput == "1") {
-                    cout << npcName << ": It's about to get even worse." << endl;
+                    printLetterByLetter(npcName + ": It's about to get even worse." );
                     return;
                 }
                 else if (playerInput == "2") {
-                    cout << npcName << ": That's not going to work on anybody." << endl;
+                    printLetterByLetter(npcName + ": That's not going to work on anybody.");
                     return;
                 }
                 else {
-                    cout << npcName << ": I'm sorry, what are you trying to say?" << endl;
-                    cout << "Omnipresent Voice: CAN YOU PLEASE STOP PRESSING RANDOM BUTTONS. Just pick either a 1 or a 2." << endl;
+                    printLetterByLetter(npcName + ": I'm sorry, what are you trying to say?" );
+                    printLetterByLetter( "Omnipresent Voice: CAN YOU PLEASE STOP PRESSING RANDOM BUTTONS. Just pick either a 1 or a 2." );
                 }
             }
         }
         else if (playerInput == "2") {
-            cout << npcName << ": Hmm... this sure doesn't seem like it's yours. Do you have any ID?" << endl;
+            printLetterByLetter(npcName + ": Hmm... this sure doesn't seem like it's yours. Do you have any ID?" );
 
             validInput = false;
             while (!validInput) {
@@ -265,33 +265,33 @@ void Gate::enter() {
                 getline(cin, playerInput);
 
                 if (playerInput == "1") {
-                    cout << npcName << ": No identification, no entry." << endl;
+                    printLetterByLetter(npcName + ": No identification, no entry." );
                     return;
                 }
                 else if (playerInput == "2") {
                     srand(static_cast<unsigned int>(time(0)));
                     int randomValue = rand() % 2;
                     if (randomValue == 0) {
-                        cout << npcName << ": Oh of course! You seem so confident I don't even have to check!" << endl;
-                        cout << "Please go right ahead." << endl;
+                        printLetterByLetter(npcName + ": Oh of course! You seem so confident I don't even have to check!" );
+                        printLetterByLetter( "Please go right ahead." );
                         return;
                         // Send them to floor 5 without fighting
                     }
                     else {
-                        cout << npcName << ": This is a half filled out rewards card for Jersey Mike's." << endl;
-                        cout << "Omnipresent Voice: Uh oh." << endl;
+                        printLetterByLetter(npcName + ": This is a half filled out rewards card for Jersey Mike's." );
+                        printLetterByLetter( "Omnipresent Voice: Did you honestly think that would work?" );
                         return;
                         // FIGHT
                     }
                 }
                 else {
-                    cout << npcName << ": I don't have time for this nonsense" << endl;
-                    cout << "Omnipresent Voice: We have been together so long and you still refuse to press the right buttons." << endl;
+                    printLetterByLetter(npcName + ": I don't have time for this nonsense" );
+                    printLetterByLetter(npcName + "Omnipresent Voice: We have been together so long and you still refuse to press the right buttons." );
                 }
             }
         }
         else if (playerInput == "3" && hasFirstClassBoardingPass) {
-            cout << npcName << ": Oh, a VIP! Please right this way. Your seat is 0A!" << endl;
+            printLetterByLetter(npcName + ": Oh, a VIP! Please right this way. Your seat is 0A!" );
 
             validInput = false;
             while (!validInput) {
@@ -299,19 +299,19 @@ void Gate::enter() {
                 cout << "1. What's 0A?" << endl;
                 cout << "2. Where's that?" << endl;
                 if (playerInput == "1" || playerInput == "2") {
-                    cout << npcName << ": Well thats the Captain's seat of course!" << endl;
+                    printLetterByLetter(npcName + ": Well thats the Captain's seat of course!" );
                     return;
                 }
                 else {
-                    cout << npcName << "Pardon?" << endl;
-                    cout << "Seriously? This late in the game?" << endl;
+                    printLetterByLetter(npcName + "Pardon?" );
+                    printLetterByLetter( "Omnipresent Voice: Seriously? This late in the game?" );
 
                 }
             }
         }
         else {
-            cout << npcName << ": I didn't quite catch that." << endl;
-            cout << "Omnipresent Voice: CAN YOU PLEASE STOP PRESSING RANDOM BUTTONS. Just pick either a 1 or a 2." << endl;
+            printLetterByLetter(npcName + ": I didn't quite catch that." );
+            printLetterByLetter( "Omnipresent Voice: CAN YOU PLEASE STOP PRESSING RANDOM BUTTONS. Just pick either a 1 or a 2." );
         }
     }
 }
@@ -325,11 +325,11 @@ Plane::Plane(bool hasStylishHat) : hasStylishHat(hasStylishHat){
 }
 
 void Plane::enter() {
-	cout << description << endl;
-	cout << "This is the last obstacle in your path. Usually I would ask you to not get into a fight here, but I honestly doubt that a choice in this scenario." << endl;
-	cout << "Good Luck, Adventurer!" << endl;
-	cout << npcName << ": My plane my rules! Rule number 1 is that I hate you and you can't fly on my plane." << endl;
-	cout << "Rule number 2 is that there are no further rules." << endl;
+    printLetterByLetter( description );
+    printLetterByLetter("This is the last obstacle in your path. Usually I would ask you to not get into a fight here, but I honestly doubt that a choice in this scenario." );
+    printLetterByLetter( "Good Luck, Adventurer!" );
+    printLetterByLetter( npcName + ": My plane my rules! Rule number 1 is that I hate you and you can't fly on my plane." );
+    printLetterByLetter("Rule number 2 is that there are no further rules." );
 
 
 	string playerInput;
@@ -347,25 +347,25 @@ void Plane::enter() {
 		getline(cin, playerInput);
 
 		if (playerInput == "1") {
-			cout << npcName << ": Sucks to suck dumbass." << endl;
+            printLetterByLetter( npcName + ": Sucks to suck dumbass." );
 			return;
 			}
 		else if (playerInput == "2") {
-			cout << npcName << "BRING IT ONNNNNNNNN!!!!" << endl;
+            printLetterByLetter(npcName + "BRING IT ONNNNNNNNN!!!!" );
 			return;
 		}
 		else if (playerInput == "3") {
-			cout << "*you lunge at The Captain's neck*" << endl;
+            printLetterByLetter( "*you lunge at The Captain's neck*" );
 			return;
 		}
 		else if (playerInput == "4" && hasStylishHat) {
-			cout << "Omnipresent Voice: That was never going to work." << endl;
-			cout << npcName << "Hmm ... No I think I'll beat you up now" << endl;
+			printLetterByLetter( "Omnipresent Voice: That was never going to work." );
+            printLetterByLetter(npcName + "Hmm ... No I think I'll beat you up now" );
 			return;
 		}
 		else {
-			cout << npcName << "What the hell are you talking about." << endl;
-			cout << "Omnipresent Voice: Bruh" << endl;
+            printLetterByLetter(npcName + "What the hell are you talking about." );
+			printLetterByLetter( "Omnipresent Voice: Bruh" );
 		}
 	}
 }
